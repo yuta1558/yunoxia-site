3cf4dfd1f88731078be09c8deb89f960cc036248
yuuuuuu1558 <58000602+yuta1558@users.noreply.github.com>
2025-06-08 00:29:15 +0900

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.style.animationPlayState = 'running';
-        observer.unobserve(entry.target);
-      }
-    });
-  }, { threshold: 0.1 });
-
-  document.querySelectorAll("[data-animate]").forEach(el => {
-    el.style.animationPlayState = 'paused';
-    observer.observe(el);
-  });
-
   const checkbox = document.getElementById("theme-toggle");
   const userTheme = localStorage.getItem("theme");
   const systemDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
