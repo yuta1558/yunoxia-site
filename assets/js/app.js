acf4d1b37aae945f2aa5a9d2b9461e327ab23ff8
yuuuuuu1558 <58000602+yuta1558@users.noreply.github.com>
2025-06-13 21:57:27 +0900

    Handle missing closest

--- assets/js/app.js
+++ assets/js/app.js
@@ -121,6 +121,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const initLinks = () => {
     if (linkHandler) document.removeEventListener("click", linkHandler);
     linkHandler = (e) => {
+      if (!e.target.closest) return;
       const link = e.target.closest("a:not([target]):not([href^='#'])");
       if (!link) return;
       e.preventDefault();
